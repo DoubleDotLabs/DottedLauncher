f7ed318140747e76776a2be30fb3f6a82d5402d1
Marlon Jones <marlonjones234@gmail.com>
2016-10-08 19:57:55 -0500

    More work - NOTE: ERRORS EVERYWHERE, DOES NOT BUILD

--- app/src/main/java/com/marlonjones/voidlauncher/homescreen.java
+++ app/src/main/java/com/marlonjones/voidlauncher/homescreen.java
@@ -3,11 +3,11 @@ package com.marlonjones.voidlauncher;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.homescreen);
     }
 }
